fd28f79c2c72dfc0c11e328f16d0f0ff81014bd7
Pahri_D0221041_C <99809141+Pahri02@users.noreply.github.com>
2022-02-22 21:03:15 +0800

    Add files via upload

---
+++ day2_stdin_and_stdout_I_java.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pahri;
+
+/**
+ *
+ * @author user
+ */
+public class day2_stdin_and_stdout_I_java {
+    
+}
